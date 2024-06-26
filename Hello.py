970605f109b1932c86ea646761d337c67a86452b
Vivek191089 <chowdary.9009@gmail.com>
2024-06-26 15:00:15 +0530

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,19 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-st.write("Hello Blue Power")
-st.write("Get visual based on movie name. contact CKU for info")
-x = st.text_input("Movie", "Star Wars")
-
-if st.button("Submit"):
-    st.write(f"Your favorite movie is {x}")
-
-
-data = pd.read_csv("movies.csv")
-st.write(data)
-
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-
-st.bar_chart(chart_data)
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
